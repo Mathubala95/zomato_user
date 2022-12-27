f5a5ce1f1df0364e83dbf92760ab512720ca145c
Mathubala95 <madhuvalar.govindaraj@gmail.com>
2022-12-27 11:39:50 +0530

    initial

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Home() {  
+  return (
+    <div id="home" class="intro route bg-image">
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          
+          <p class="intro-subtitle"><span>... Site is Under Construction ...</span><strong class="text-slider"></strong></p>
+         
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Home
